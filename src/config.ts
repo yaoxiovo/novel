ab00943a8b31ed5dff4295284166e27c37ab6038
yaoxiovo <yaoxiovo@gmail.com>
2026-05-23 10:11:37 +0800

    1

--- src/config.ts
+++ src/config.ts
@@ -117,7 +117,7 @@ export const navBarConfig: NavBarConfig = {
 		},
 		{
 			name: "统计",
-			url: "https://cloud.umami.is/share/ZCxAbIrILX8idys4", // Internal links should not include the base path, as it is automatically added
+			url: "https://umami.yaoxi.cloud/share/9ptO9jjAHNFelZxM", // Internal links should not include the base path, as it is automatically added
 			external: true, // Show an external link icon and will open in a new tab
 		},
 			
@@ -156,8 +156,8 @@ export const imageFallbackConfig: ImageFallbackConfig = {
 
 export const umamiConfig: UmamiConfig = {
 	enable: true,
-	baseUrl: "https://cloud.umami.is",
-	shareId: "ZCxAbIrILX8idys4",
+	baseUrl: "https://umami.yaoxi.cloud",
+	shareId: "9ptO9jjAHNFelZxM",
 	timezone: "Asia/Shanghai",
 };
 
